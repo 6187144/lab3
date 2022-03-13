74888e157985e5517fc1f71e3d64ea9876df6444
6187144 <6187144@cegep-heritage.qc.ca>
2022-03-13 14:16:11 -0400

    hopefully final version

--- app.js
+++ app.js
@@ -3,46 +3,22 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-const bobber = require('./middleware/addBob');
+const queryString = require('querystring');
 
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+middleware = function (req, res, next) {
+    username = req.query.uname;
+    if (!username) return next();
+    let user = username.toLowerCase().trim();
+    if (!user) return next();
+    res.send('uname is: ' + user);
+}
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use('/', express.static(path.join(__dirname, 'public')));
-app.use(bobber);
-app.use((req, res, next) => {
-    console.log("received request: ", req.get('host') + req.originalUrl);
-    next();
-});
-app.use('/', indexRouter);
-
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
+app.get("/", middleware, function (req, res) {
+    res.send("No valid query string for uname");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
 
 module.exports = app;
